e0fe8f99074290dd1421fbe7348de6be941b347f
Hayley Robinson <hayley.robinson63@myhunter.cuny.edu>
2022-01-10 09:54:26 -0500

    MVC for story line

--- Destini-iOS13/Controller/ViewController.swift
+++ Destini-iOS13/Controller/ViewController.swift
@@ -13,12 +13,27 @@ class ViewController: UIViewController {
     @IBOutlet weak var storyLabel: UILabel!
     @IBOutlet weak var choice1Button: UIButton!
     @IBOutlet weak var choice2Button: UIButton!
-    
+
+    var storyBrain = StoryBrain()
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        updateUI()
     }
 
-
+    @IBAction func choiceMade(_ sender: UIButton) {
+        let userAnswer = sender.currentTitle!
+        storyBrain.nextStory(userAnswer)
+        
+        updateUI()
+        
+    }
+    
+    @objc func updateUI(){
+        storyLabel.text = storyBrain.getQuestion()
+        choice1Button.setTitle(storyBrain.getChoice1(), for: .normal)
+        choice2Button.setTitle(storyBrain.getChoice2(), for: .normal)
+    }
+    
 }
 
